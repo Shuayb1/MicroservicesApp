4a109d4ba221d3e038d98580a72ff3aa5b8c1738
Shuayb Popoola <shuaybpopoola@gmail.com>
2021-11-10 05:44:38 +0100

    Added Web App for frontend

---
+++ src/WebApp/AspnetRunBasics/ApiCollection/Infrastructure/ApiBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Infrastructure
+{
+    public class ApiBuilder
+    {
+    }
+}
